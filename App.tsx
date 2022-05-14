50e6d982b51fc2c7f945fa8e9112046686384878
jaredjj3 <jared@jaredjohnson.dev>
2022-05-14 09:01:25 -0400

    Pare down dependencies

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import './style.css';
+import { Button } from '@mui/material';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1 >Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
+      <Button>foo</Button>
     </div>
   );
 }
--- index.tsx
+++ index.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
